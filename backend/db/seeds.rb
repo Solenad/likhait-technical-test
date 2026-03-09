18cce4fba486e3efa90572d6ecf9c3e415c20de9
Solenad <roe.dizon80@gmail.com>
2026-03-09 19:16:59 +0800

    chore: temp changed expense instances to 500

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -113,7 +113,8 @@ expense_templates = {
 
 # Start date: January 1, 2024
 # End date: February 18, 2026
-start_date = Date.new(2024, 1, 1)
+# IF DEV: Start date = January 1, 2026
+start_date = Date.new(2026, 1, 1)
 end_date = Date.new(2026, 2, 18)
 
 expense_count = 0
@@ -141,6 +142,7 @@ while current_date <= end_date
       amount += rand(0..99) / 100.0
 
       # Create the expense with created_at set to the date
+      # FIX LATER: build a hash, then insert once into database
       Expense.create!(
         description: template[:description],
         amount: amount,
